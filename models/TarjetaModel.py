961c498929730b247bb612c298a7bfbb4f964184
rduguet <reinaldo@fraccion.cl>
2023-11-22 03:45:19 -0300

    metodos para suscribir y listar tarjetas

--- models/TarjetaModel.py
+++ models/TarjetaModel.py
@@ -7,26 +7,72 @@ import ipdb
 class TarjetaModel():
 
     @classmethod
-    def get_tarjeta(self):
+    def get_tarjetas(self):
         try:
             connection = get_connection()
             tarjetas = []
-
             with connection.cursor() as cursor:
                 cursor.execute("SELECT id, username, email, response_url from cliente ORDER BY id ASC")
                 resultset = cursor.fetchall()
-
                 for row in resultset:
-                    #ipdb.set_trace()
                     tarjeta = Tarjeta(row[0],row[1],row[2],row[3])
-                    if validarRut(row[1]):
+                    if validarRut(row[1]):   # DE ESTE MODO SOLO TRAE LOS RUTs VALIDOS
                         tarjetas.append(tarjeta.to_JSON())
-                    #tarjetas.append(tarjeta.to_JSON())
                     else:
                         pass
+            connection.close()
+            return tarjetas
+        except Exception as ex:
+            raise Exception(ex)
 
+
+    @classmethod
+    def get_tarjeta(self,username):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT id, username, email, response_url from cliente WHERE username = %s",(username,))
+                row = cursor.fetchone()
+                print(row)
+                tarjeta = None
+                if row != None:
+                    tarjeta = Tarjeta(row[0],row[1],row[2],row[3])
+                    tarjeta = tarjeta.to_JSON()
+            connection.close()
+
+            return tarjeta
+        except Exception as ex:
+            raise Exception(ex)
+
+    @classmethod
+    def get_muchas_tarjetas(self,username):
+        try:
+            connection = get_connection()
+            tarjetas = []
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT id, username, email, response_url from cliente WHERE username = %s",(username,))
+                resultset = cursor.fetchall()
+                for row in resultset:
+                    tarjeta = Tarjeta(row[0],row[1],row[2],row[3])
+                    tarjetas.append(tarjeta.to_JSON())
 
             connection.close()
             return tarjetas
+        except Exception as ex:
+            raise Exception(ex)
+
+
+    @classmethod
+    def post_tarjeta(self,tarjeta):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute("""INSERT INTO cliente (id,username,email,response_url)
+                                VALUES (%s,%s,%s,%s)""",(tarjeta.id,tarjeta.username,tarjeta.email,tarjeta.response_url))
+                affected_rows = cursor.rowcount
+                connection.commit()
+
+            connection.close()
+            return tarjeta
         except Exception as ex:
             raise Exception(ex)
--- routes/oneClick.py
+++ routes/oneClick.py
@@ -1,6 +1,10 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint,jsonify, request
 import ipdb
+import uuid
 
+#entities
+from models.entities.tarjeta import Tarjeta
+#modelo(s)
 from models.TarjetaModel import TarjetaModel
 
 
@@ -10,7 +14,49 @@ main = Blueprint('oneClickTbk',__name__) #el nombre que va en el primer argument
 @main.route('/')
 def get_cards():
     try:
-        tarjetas = TarjetaModel.get_tarjeta()
+        tarjetas = TarjetaModel.get_tarjetas()
         return jsonify(tarjetas)
+    except Exception as ex:
+        return jsonify({'mensaje':str(ex)}), 500
+
+@main.route('/<username>')
+def get_card(username):
+    try:
+        tarjeta = TarjetaModel.get_tarjeta(username)
+        if tarjeta != None:
+            return jsonify(tarjeta)
+        else:
+            return jsonify({}), 400
+    except Exception as ex:
+        return jsonify({'mensaje':str(ex)}), 500
+
+@main.route('cards/<username>')
+def get_too_cards(username):
+    try:
+        tarjetas = TarjetaModel.get_muchas_tarjetas(username)
+        print(tarjetas)
+        return jsonify(tarjetas)
+    except Exception as ex:
+        return jsonify({'mensaje':str(ex)}), 500
+
+
+@main.route('/suscribe', methods=['POST'])
+def post_card():
+    try:
+        #id           = uuid.uuid4()
+        id           = request.json['id']
+        username     = request.json['username']
+        email        = request.json['email']
+        response_url = request.json['response_url']
+        tarjeta = Tarjeta(id,username,email,response_url)
+
+        affected_rows = TarjetaModel.post_tarjeta(tarjeta)
+        print(affected_rows)
+
+        if affected_rows == 1 :
+            return jsonify(tarjeta.id)
+
+        return jsonify({"msje":"Error al insertar valor en bbdd"})
+
     except Exception as ex:
         return jsonify({'mensaje':str(ex)}), 500
